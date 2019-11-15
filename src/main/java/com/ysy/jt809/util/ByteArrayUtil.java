9062f04b2a2957dc2a7b27af74621ed90149fd75
lustres@sina.com <lustres@sina.com>
2019-11-15 10:14:46 +0800

    修改名称

--- src/main/java/com/ysy/jt809/util/ByteArrayUtil.java
+++ src/main/java/com/ysy/jt809/util/ByteArrayUtil.java
@@ -4,16 +4,20 @@ import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * @author Administrator
+ */
 public class ByteArrayUtil {
 
     /**
      * byte数组拼接
-     * @param first
-     * @param back
-     * @return
+     *
+     * @param first first
+     * @param back  back
+     * @return byte[]
      */
     public static byte[] append(byte[] first, byte[] back) {
-        if(null == first || null == back){
+        if (null == first || null == back) {
             return null;
         }
         int length = first.length + back.length;
@@ -26,12 +30,13 @@ public class ByteArrayUtil {
 
     /**
      * sub byte
-     * @param data
-     * @param off
-     * @param length
-     * @return
+     *
+     * @param data   data
+     * @param off    off
+     * @param length length
+     * @return byte[]
      */
-    public static byte[] subBytes(byte[] data,int off,int length){
+    public static byte[] subBytes(byte[] data, int off, int length) {
         byte[] res = new byte[length];
         System.arraycopy(data, off, res, 0, length);
         return res;
@@ -40,7 +45,8 @@ public class ByteArrayUtil {
 
     /**
      * short转换为byte[]
-     * @param number
+     *
+     * @param number number
      * @return byte[]
      */
     public static byte[] short2Bytes(short number) {
@@ -52,19 +58,20 @@ public class ByteArrayUtil {
 
     /**
      * byte[]转换为short
-     * @param bytes
+     *
+     * @param bytes bytes
      * @return short
      */
-    public static short bytes2Short(byte[] bytes){
-        short z = (short)((bytes[0] << 8) | (bytes[1] & 0xFF));
+    public static short bytes2Short(byte[] bytes) {
+        short z = (short) ((bytes[0] << 8) | (bytes[1] & 0xFF));
         return z;
     }
 
     /**
      * byte to int
      *
-     * @param data
-     * @return
+     * @param data data
+     * @return int
      */
     public static int bytes2int(byte[] data) {
         int mask = 0xff;
@@ -82,8 +89,8 @@ public class ByteArrayUtil {
     /**
      * int to bytes
      *
-     * @param n
-     * @return
+     * @param n n
+     * @return byte[]
      */
     public static byte[] int2bytes(int n) {
         byte[] b = new byte[4];
@@ -97,18 +104,20 @@ public class ByteArrayUtil {
 
     /**
      * byte[] 转换为 GBK 编码的 字符串
-     * @param byteArray
-     * @return
+     *
+     * @param byteArray byteArray
+     * @return String
      */
     public static String bytes2gbkString(byte[] byteArray) {
-        return bytes2string(byteArray,"GBK");
+        return bytes2string(byteArray, "GBK");
     }
 
     /**
      * byte[] 转换为 string
-     * @param byteArray
-     * @param charset
-     * @return
+     *
+     * @param byteArray byteArray
+     * @param charset charset
+     * @return String
      */
     public static String bytes2string(byte[] byteArray, String charset) {
         if (byteArray.length == 0) {
@@ -119,8 +128,9 @@ public class ByteArrayUtil {
 
     /**
      * byte[] 转换为 string
-     * @param byteArray
-     * @return
+     *
+     * @param byteArray byteArray
+     * @return String
      */
     public static String bytes2string(byte[] byteArray) {
         if (byteArray.length == 0) {
@@ -132,13 +142,14 @@ public class ByteArrayUtil {
 
     /**
      * byte[] to float
-     * @param floatArray
-     * @return
+     *
+     * @param floatArray floatArray
+     * @return float
      */
-    public static float bytes2float(byte[] floatArray){
+    public static float bytes2float(byte[] floatArray) {
         int accum = 0;
-        for(int i = 0,j = 0; i < floatArray.length; i ++){
-            accum = accum|(floatArray[i] & 0xff) << j;
+        for (int i = 0, j = 0; i < floatArray.length; i++) {
+            accum = accum | (floatArray[i] & 0xff) << j;
             j += 8;
         }
         return Float.intBitsToFloat(accum);
@@ -147,8 +158,9 @@ public class ByteArrayUtil {
 
     /**
      * byte[] to double
-     * @param doubleArray
-     * @return
+     *
+     * @param doubleArray doubleArray
+     * @return double
      */
     public static double bytes2double(byte[] doubleArray) {
         long value = 0;
@@ -160,15 +172,16 @@ public class ByteArrayUtil {
 
     /**
      * 数组转换成十六进制字符串
-     * @param array
+     *
+     * @param array array
      * @return HexString
      */
     public static String bytes2HexStr(byte[] array) {
         StringBuffer sb = new StringBuffer(array.length);
         String sTemp;
-        for (int i = 0; i < array.length; i++) {
-            sTemp = Integer.toHexString(0xFF & array[i]);
-            if (sTemp.length() < 2){
+        for (byte b : array) {
+            sTemp = Integer.toHexString(0xFF & b);
+            if (sTemp.length() < 2) {
                 sb.append(0);
             }
             sb.append(sTemp.toUpperCase());
@@ -178,7 +191,8 @@ public class ByteArrayUtil {
 
     /**
      * 数组转换成带0x的十六进制字符串
-     * @param array
+     *
+     * @param array array
      * @return HexString
      */
     public static String bytes2FullHexStr(byte[] array) {
@@ -187,11 +201,11 @@ public class ByteArrayUtil {
         String sTemp;
         for (int i = 0; i < array.length; i++) {
             sTemp = Integer.toHexString(0xFF & array[i]);
-            if (sTemp.length() < 2){
+            if (sTemp.length() < 2) {
                 sb.append(0);
             }
             sb.append(sTemp);
-            if(i < array.length-1){
+            if (i < array.length - 1) {
                 sb.append("0x");
             }
         }
@@ -200,16 +214,18 @@ public class ByteArrayUtil {
 
     /**
      * short to 16进制字符串
-     * @param num
-     * @return
+     *
+     * @param num num
+     * @return String
      */
-    public static String short2HexStr(short num){
+    public static String short2HexStr(short num) {
         return Integer.toHexString(num);
     }
 
     /**
      * 把16进制字符串转换成字节数组
-     * @param hex
+     *
+     * @param hex hex
      * @return byte[]
      */
     public static byte[] hexStr2Bytes(String hex) {
@@ -225,55 +241,53 @@ public class ByteArrayUtil {
 
     /**
      * 把带0x的16进制字符串转换成字节数组
-     * @param hex
+     *
+     * @param hex hex
      * @return byte[]
      */
-    public static byte[] fullHexStr2Bytes(String hex){
-        hex = hex.toLowerCase().replaceAll("0x","").trim().toUpperCase();
+    public static byte[] fullHexStr2Bytes(String hex) {
+        hex = hex.toLowerCase().replaceAll("0x", "").trim().toUpperCase();
         return hexStr2Bytes(hex);
     }
 
     private static int toByte(char c) {
-        byte b = (byte) "0123456789ABCDEF".indexOf(c);
-        return b;
+        return (byte) "0123456789ABCDEF".indexOf(c);
     }
 
     /**
      * byte数组转时间字符串 格式 yyMMddHHmmss
-     * @return
+     *
+     * @return String
      */
-    public static String bytes2timeStr(byte[] array){
+    public static String bytes2timeStr(byte[] array) {
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0; i < array.length; i ++){
-            int timeUnit = byte2UnsignedInt(array[i]);
-            if(timeUnit < 10){
+        for (byte b : array) {
+            int timeUnit = byte2UnsignedInt(b);
+            if (timeUnit < 10) {
                 stringBuilder.append(0);
             }
             stringBuilder.append(timeUnit);
         }
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-        return  sdf.format(new Date()).substring(0,2) + stringBuilder.toString();
+        return sdf.format(new Date()).substring(0, 2) + stringBuilder.toString();
     }
 
 
     /**
      * byte转无符号整数
-     * @param value
-     * @return
+     *
+     * @param value value
+     * @return int
      */
     public static int byte2UnsignedInt(byte value) {
         return Byte.toUnsignedInt(value);
     }
 
     /**
+     * 将一个long转换成8位的byte[]
      *
-     *
-     * @描述 将一个long转换成8位的byte[]
-     * @param num
-     * 	long值
-     * @return
-     * 长度是8的byte[]
-     * @throws Exception
+     * @param num long值
+     * @return 长度是8的byte[]
      */
     public static byte[] longToBytes(long num) {
         byte[] b = new byte[8];
@@ -282,15 +296,12 @@ public class ByteArrayUtil {
         }
         return b;
     }
+
     /**
+     * 将一个数组转换成一个long值
      *
-     *
-     * @描述 将一个数组转换成一个long值
-     * @param b
-     *  长度是8的byte[]
-     * @return
-     * 	long值
-     * @throws Exception
+     * @param b 长度是8的byte[]
+     * @return long值
      */
     public static long bytesToLong(byte[] b) {
         int mask = 0xff;
@@ -305,11 +316,12 @@ public class ByteArrayUtil {
     }
 
     /**
-     *  将一个byte数组转换成二进制字符串
-     * @param bytes
+     * 将一个byte数组转换成二进制字符串
+     *
+     * @param bytes bytes
      * @return 二进制字符串
      */
-    public static String bytes2bitStr(byte[] bytes){
+    public static String bytes2bitStr(byte[] bytes) {
         StringBuilder stringBuilder = new StringBuilder();
         for (byte b : bytes) {
             stringBuilder.append(byte2bitStr(b));
@@ -318,8 +330,9 @@ public class ByteArrayUtil {
     }
 
     /**
-     *  将一个byte转换成二进制字符串
-     * @param b
+     * 将一个byte转换成二进制字符串
+     *
+     * @param b b
      * @return 二进制字符串
      */
     public static String byte2bitStr(byte b) {
@@ -328,10 +341,6 @@ public class ByteArrayUtil {
                 + (byte) ((b >> 3) & 0x1) + (byte) ((b >> 2) & 0x1)
                 + (byte) ((b >> 1) & 0x1) + (byte) ((b >> 0) & 0x1);
     }
-
-
-
-
 
 
 }
